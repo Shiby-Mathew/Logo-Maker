4566bacfaf32c38b7f82f7910d1893182e1024f3
Shiby-Mathew <shiby.ajay@gmail.com>
2023-04-19 19:47:23 +1000

    installed npm inquirer and prompt working

--- index.js
+++ index.js
@@ -6,8 +6,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-
-
 //main function
 const main = () => {
   //prompt asking for user inputs
@@ -28,12 +26,12 @@ const main = () => {
         type: "list",
         name: "shape",
         message: "Please select shape of your logo",
-        choice: ["Triangle", "Square", "Circle"],
+        choices: ["Triangle", "Square", "Circle"],
       },
 
       {
         type: "list",
-        name: "charcolor",
+        name: "background",
         message: "Select background color for your logo",
         choices: ["White", "Black", "Blue"],
       },
@@ -51,3 +49,76 @@ const main = () => {
     });
 };
 main();
+// const inquirer = require("inquirer");
+// const fs = require("fs");
+
+// //their name, location, bio, LinkedIn URL, and GitHub URL
+// const main = () => {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         message: "What is your user name?",
+//         name: "username",
+//       },
+//       {
+//         type: "input",
+//         message: "Where is your location",
+//         name: "location",
+//       },
+//       {
+//         type: "input",
+//         message: "Your Biodata ",
+//         name: "bio",
+//       },
+//       {
+//         type: "input",
+//         message: " LinkedIn URL",
+//         name: "linkedIn",
+//       },
+//       {
+//         type: "input",
+//         message: " GitHub URL",
+//         name: "gitHub",
+//       },
+//     ])
+
+//     .then((data) => {
+//       // console.log(data);
+
+//       const { username, location, bio, linkedIn, gitHub } = data;
+//       const generateHTML = ` <!DOCTYPE html>
+//     <html lang="en-US">
+//       <head>
+//         <meta charset="UTF-8" />
+//         <title>Landing Page</title>
+//       </head>
+
+//       <body>
+//         <div>
+//           <header>
+//            <h1>My name is ${username}</h1>
+//           </header>
+//           <section>
+//           <p>Location: ${location} </p>
+//           <p>Biodata: ${bio}</p>
+//           </section>
+
+//           <section>
+//           <p>GitHub URL: <a href="${gitHub}">Check my gitHub</a></p>
+//           <p>LinkedIn URL:  <a href="${linkedIn}">Check myLinkedIn profile</a></p>
+//           </section>
+
+//           <footer>
+//             <p> Copyright &copy; 2023 </p>
+//           </footer>
+//         </div>
+//       </body>
+//     </html>
+//     `;
+//       fs.writeFile("index.html", generateHTML, (err) => {
+//         err ? console.error(err) : console.log("success");
+//       });
+//     });
+// };
+// main();
