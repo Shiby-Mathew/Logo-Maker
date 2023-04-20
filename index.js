45137c59a8915049bcf7f58f2bff020e7faf1f5d
Shiby-Mathew <shiby.ajay@gmail.com>
2023-04-20 21:37:38 +1000

    delete package-lock.json

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //import class from shape.js
 //import questions from questions.js
-//import Inquirer
-//import fs//
+
 
 //function to make logo using these values
 const Questions = require("./lib/questions.js");
