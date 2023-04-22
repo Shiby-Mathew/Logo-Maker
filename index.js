eab33b5559d7782216d4b7be97325533a56d13d4
Shiby-Mathew <shiby.ajay@gmail.com>
2023-04-22 11:43:31 +1000

    changes in the coordinates

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//import class from shape.js
+
 //import questions from questions.js
 
 
-//function to make logo using these values
+
 const Questions = require("./lib/questions.js");
 
 const questions = new Questions();
